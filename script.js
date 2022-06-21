f96a9a81827ede9981bf402237fa9c78238832ea
MonicaAlexis <osikhuemhemonica2017@.com>
2022-06-22 05:53:54 +0800

    js_output

---
+++ script.js
@@ -0,0 +1,7 @@
+var myName = "Osikhuemhe Monica Ilamosi"
+var height = 5.1
+var country = "Nigeria"
+
+document.write('My name is' + ' ' + myName + '<br>')
+document.write("I'm "+ ' ' + height + ' ' +'feet tall' +'<br>')
+document.write("I'm from" + ' ' + country)
